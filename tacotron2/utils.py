e94308ba95935e6658cac4237beb039694bcd312
AntiEntropy <67179171+Anti-Entrophic@users.noreply.github.com>
2023-02-12 23:22:27 +0800

    Update utils.py

--- tacotron2/utils.py
+++ tacotron2/utils.py
@@ -20,22 +20,21 @@ def get_mask_from_lengths(lengths):
     
     return mask
 
-
-
 def load_wav_to_torch(full_path):
     sampling_rate, data = read(full_path)
     return torch.FloatTensor(data.astype(np.float32)), sampling_rate
 
 
-def load_filepaths_and_text(filename, split="|"):
-    with open(filename, encoding='utf-8') as f:
-        filepaths_and_text = [line.strip().split(split) for line in f]
-    return filepaths_and_text
-
 def load_filepaths_and_PPG(filename, split="|"):
     with open(filename, encoding='utf-8') as f:
         filepaths_and_PPG = [line.strip().split(split) for line in f]
     return filepaths_and_PPG
+
+# deprecated
+def load_filepaths_and_text(filename, split="|"):
+    with open(filename, encoding='utf-8') as f:
+        filepaths_and_text = [line.strip().split(split) for line in f]
+    return filepaths_and_text
 
 def to_gpu(x):
     x = x.contiguous()
