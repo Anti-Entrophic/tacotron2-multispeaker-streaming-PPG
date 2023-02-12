93af977e6d64c4e3317659ee016901cd19e9a473
AntiEntropy <67179171+Anti-Entrophic@users.noreply.github.com>
2023-02-12 23:21:43 +0800

    Update utils.py

--- tacotron2/utils.py
+++ tacotron2/utils.py
@@ -32,10 +32,10 @@ def load_filepaths_and_text(filename, split="|"):
         filepaths_and_text = [line.strip().split(split) for line in f]
     return filepaths_and_text
 
-def load_filepaths_and_PPG_and_id(filename, split="|"):
+def load_filepaths_and_PPG(filename, split="|"):
     with open(filename, encoding='utf-8') as f:
-        filepaths_and_PPG_and_id = [line.strip().split(split) for line in f]
-    return filepaths_and_PPG_and_id
+        filepaths_and_PPG = [line.strip().split(split) for line in f]
+    return filepaths_and_PPG
 
 def to_gpu(x):
     x = x.contiguous()
